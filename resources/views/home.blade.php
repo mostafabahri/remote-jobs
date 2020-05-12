f3356c7b327ac3d34694ef18455e04c54fd34212
Mostafa Bahri <mostafabahri.dev@gmail.com>
2020-05-12 15:10:00 +0430

    Update home with basic header

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>We Work Remotely</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+</head>
+
+<body>
+    <div class="text-center mt-10 px-10">
+        <header class="max-w-4xl mx-auto space-y-8">
+            <h1 class="text-5xl font-bold">We Work Remotely</h1>
+            <h3 class="text-2xl text-gray-600">We Work Remotely is the <span class="font-medium text-gray-900">largest
+                    remote work community in the world</span>. With over 2.5M monthly visitors, WWR is the #1
+                destination to find and list incredible remote jobs.</h3>
+            <button href="#" class="bg-red-600 text-white px-8 py-3 hover:shadow-lg font-medium">Post a job for
+                FREE!</button>
+        </header>
+    </div>
+</body>
+
+</html>
