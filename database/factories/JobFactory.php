9a531c23cbe2514bb338845ad1e9c1dfc19df2ca
Mostafa Bahri <mostafabahri.dev@gmail.com>
2020-05-18 16:56:37 +0430

    Update JobFactory

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -6,10 +6,14 @@ use App\Job;
 use Faker\Generator as Faker;
 
 $factory->define(Job::class, function (Faker $faker) {
+    $time = $faker->dateTimeBetween('-1 month');
+
     return [
        'title' => $faker->sentence(4),
         'region' => $faker->randomElement([
             'Full Time/Anywhere', 'Only USA', 'Only Europe'
         ]),
+        'created_at' => $time,
+        'updated_at' => $time
     ];
 });
