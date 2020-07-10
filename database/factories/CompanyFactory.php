7cb32e71ce967341e0a6fc5922f5a571862d6b46
Mostafa Bahri <mostafabahri.dev@gmail.com>
2020-07-11 01:08:39 +0430

    Use fixed company seed logo

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -8,7 +8,6 @@ use Faker\Generator as Faker;
 $factory->define(Company::class, function (Faker $faker) {
     return [
        'name' => $faker->company,
-       'logo' => $faker->boolean(70) ? $faker->picsumUrl(50, 50): null
-
+       'logo' => $faker->boolean(70) ? 'https://picsum.photos/seed/' . $faker->numberBetween(1, 1e6) . '/100/100': null
     ];
 });
