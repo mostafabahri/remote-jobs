3df666487881125bfc14b9301461af16213b7329
Mostafa Bahri <mostafabahri@git.commit>
2020-09-28 15:25:38 +0330

    Use job-create route in template

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -7,6 +7,6 @@
     <div class="max-w-4xl mx-auto space-y-8 my-5 py-10 px-5">
         <h1 class="text-4xl lg:text-5xl font-bold">{{$title}}</h1>
         <h3 class="text-lg lg:text-xl text-gray-600">{{$description }}</h3>
-        <x-button>{{$cta}}</x-button>
+        <x-button href="{{route('jobs.create')}}">{{$cta}}</x-button>
     </div>
 </header>
