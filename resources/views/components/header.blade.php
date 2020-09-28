0b0bdcb69504c8873b75c945dd81c9e9cb8be789
Mostafa Bahri <mostafabahri@git.commit>
2020-09-28 07:47:53 +0330

    Extract common background css pattern

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,7 +3,7 @@
 'description' => '',
 'cta' => "Post a job for FREE!",
 ])
-<header class="site-header text-center border-b pt-20">
+<header class="bg-pattern text-center border-b pt-20">
     <div class="max-w-4xl mx-auto space-y-8 my-5 py-10 px-5">
         <h1 class="text-4xl lg:text-5xl font-bold">{{$title}}</h1>
         <h3 class="text-lg lg:text-xl text-gray-600">{{$description }}</h3>
