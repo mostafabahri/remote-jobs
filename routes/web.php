9c9f3b412674d46f8d45e1e0831a1e2f174dc162
Mostafa Bahri <mostafabahri@git.commit>
2020-09-28 14:49:00 +0330

    Update HomeController
    a bit of housekeeping

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController')->name('home');
 
 Route::resource('/jobs', 'JobController')->only([
     'show'
